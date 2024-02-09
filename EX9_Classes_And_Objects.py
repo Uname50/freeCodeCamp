2f696f24b1d3129683a4bc42afe63325211cdf15
Uname50 <158527626+Uname50@users.noreply.github.com>
2024-02-08 21:41:31 -0500

    milestone 2 for exercise 9

--- EX9_Classes_And_Objects.py
+++ EX9_Classes_And_Objects.py
@@ -18,3 +18,42 @@ class Board:
         lower_lines = f'╚═══{"╧═══"*2}{"╩═══"}{"╧═══"*2}{"╩═══"}{"╧═══"*2}╝\n'
 
         board_string = upper_lines
+
+# Enumeration is a convenient way to keep track of both the element and its position on a list. The enumerate() function is a built-in function in Python that takes an iterable (such as a list, tuple, or string) and returns an iterator that produces tuples containing indices and corresponding values from the iterable.
+        
+        for index, line in enumerate(self.board):
+            
+            # store the elements of a single row in the sudoku board
+            row_list = []
+            
+            # split each row in three segments in order to represent the 3x3 squares properly
+            for square_no, part in enumerate([line[:3], line[3:6], line[6:]], start = 1):
+                
+                # join the elements of the segment(part) with a pipe character                
+                # access all elements
+                row_square = '|'.join(str(item) for item in part)
+                
+                # Extend the row_list with the elements of the row_square string
+                row_list.extend(row_square) 
+
+                # check if the current segment (square_no) is not the last one
+                if square_no != 3:
+                    row_list.append('║')
+
+        # create a string representation of the row with spaces between each element
+        row = f'║ {" ".join(row_list)} ║\n' 
+
+        # replace the 0 that will be used for empty cells with a space
+        row_empty = row.replace('0', ' ')
+
+        # board_string is gradually built up as the loop iterates over each row, creating the full ASCII art representation of the sudoku board
+        board_string += row_empty
+
+        # check if the row index is 8, as it will be handled differently:
+        if index < 8:
+            
+            # verify if the row is the last row inside a 3x3 square
+            if index % 3 == 2:
+                
+                # in order to create a visually appealing border, append a different border string to board_string 
+                board_string += f'╠═══{"╪═══"*2}{"╬═══"}{"╪═══"*2}{"╬═══"}{"╪═══"*2}╣\n'
